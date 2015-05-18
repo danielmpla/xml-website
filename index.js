04bbd751ab1f0927f63b66f427a482b3f80ed97f
Daniel Müssig <danielmpla@gmail.com>
2015-05-18 10:07:08 +0200

    add registering

--- index.js
+++ index.js
@@ -19,13 +19,36 @@ monitoring.controller("controller", function($scope, uiGmapGoogleMapApi) {
 
     $scope.map = { center: { latitude: 51.1493459, longitude: 14.9978427 }, zoom: 13 };
     $scope.loggedIn = false;
+    $scope.registering = false;
 
-    $scope.login = {username: "",
-                    password: ""};
+    $scope.register = function() {
+        $scope.registering = true;
+    };
+
+    $scope.login = {
+        username: "",
+        password: ""
+    };
+
+    $scope.registerData = {
+        username: "",
+        password: "",
+        password2: ""
+    };
 
     $scope.doLogin = function() {
         $scope.loggedIn = true;
         $scope.login.password = md5($scope.login.password);
+    };
+
+    $scope.doRegister = function() {
+        if ($scope.registerData.password === $scope.registerData.password2) {
+            $scope.login.password = md5($scope.registerData.password);
+            $scope.login.username = $scope.registerData.username;
+            $scope.loggedIn = true;
+        } else {
+            alert("Die beiden Passwörter stimmen nicht überein!");
+        }
     };
 
     $scope.markers = [{
