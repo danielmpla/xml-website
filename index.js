b9aa902195b9134e014fcfce60e289adb5abf642
Daniel Müssig <danielmpla@gmail.com>
2015-06-21 17:48:20 +0200

    first testable version

--- index.js
+++ index.js
@@ -11,14 +11,7 @@ monitoring.config(function (uiGmapGoogleMapApiProvider, $httpProvider) {
     }
 );
 
-monitoring.controller("controller", ['$scope', 'uiGmapGoogleMapApi', 'userService', 'positionService', function($scope, uiGmapGoogleMapApi, userService, positionService) {
-    // Do stuff with your $scope.
-    // Note: Some of the directives require at least something to be defined originally!
-    // e.g. $scope.markers = []
-
-    // uiGmapGoogleMapApi is a promise.
-    // The "then" callback function provides the google.maps object.
-
+monitoring.controller("controller", ['$scope', 'uiGmapGoogleMapApi', 'userService', 'positionService','zonenService' , function($scope, uiGmapGoogleMapApi, userService, positionService, zonenService) {
     $scope.iconURL = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|';
     $scope.forbidden = "#ff0303";
     $scope.notForbidden = "#08B21F";
@@ -47,6 +40,75 @@ monitoring.controller("controller", ['$scope', 'uiGmapGoogleMapApi', 'userServic
 
         userService.login($scope.login.username, $scope.login.password).success(function (data) {
             $scope.loggedIn = true;
+            positionService.readAllDevices($scope.login.username, $scope.login.password)
+                .success(
+                function (data) {
+                    var parser = new DOMParser();
+                    var xmlDoc = parser.parseFromString(data,"text/xml");
+
+                    for (var i = 0; i < xmlDoc.getElementsByTagName("device").length; i++) {
+                        var device = xmlDoc.getElementsByTagName("device")[i];
+
+                        var zones = [];
+                        var zoneNodes = device.childNodes[11].getElementsByTagName("zone");
+
+                        for (var z = 0; z < zoneNodes.length; z++) {
+                            var col;
+
+                            if (zoneNodes[z].childNodes.item(3).firstChild.data === "true") {
+                                col = $scope.forbidden;
+                            } else {
+                                col = $scope.notForbidden;
+                            }
+
+                            zones.push(
+                                {
+                                    'name': zoneNodes[z].childNodes.item(1).firstChild.data,
+                                    'center': {'latitude': parseFloat(zoneNodes[z].childNodes.item(7).firstChild.data), 'longitude': parseFloat(zoneNodes[z].childNodes.item(5).firstChild.data)},
+                                    'radius': parseFloat(zoneNodes[z].childNodes.item(9).firstChild.data),
+                                    'forbidden': col,
+                                    'editable': false,
+                                    'visible': false
+                                }
+                            );
+                        }
+
+                        if (device.childNodes[9].hasChildNodes()) {
+                            $scope.clients.push(
+                                {
+                                    'id': i,
+                                    'name': device.childNodes.item(3).firstChild.data,
+                                    'mac': device.childNodes.item(1).firstChild.data,
+                                    'color': device.childNodes.item(5).firstChild.data,
+                                    'coords': {'latitude': parseFloat(device.childNodes[9].childNodes[5].childNodes[0].data), 'longitude': parseFloat(device.childNodes[9].childNodes[3].childNodes[0].data)},
+                                    'zones': zones,
+                                    'edit': false,
+                                    'visible': true,
+                                    'new': false
+                                });
+                        } else {
+                            $scope.clients.push(
+                                {
+                                    'id': i,
+                                    'name': device.childNodes.item(3).firstChild.data,
+                                    'mac': device.childNodes.item(1).firstChild.data,
+                                    'color': device.childNodes.item(5).firstChild.data,
+                                    'coords': {'latitude': 1, 'longitude': 1},
+                                    'zones': zones,
+                                    'edit': false,
+                                    'visible': true,
+                                    'new': true
+                                });
+                        }
+                    }
+                    poll();
+                }
+            )
+                .error(
+                function (data) {
+
+                }
+            );
         }).error(function (data) {
             alert("Bitte überprüfen Sie Benutzername und Passwort!")
         });
@@ -69,7 +131,7 @@ monitoring.controller("controller", ['$scope', 'uiGmapGoogleMapApi', 'userServic
         }
     };
 
-    $scope.clients = [{
+    $scope.clients = [/*{
         id: 1,
         name: "Name",
         mac: "Test MAC",
@@ -86,7 +148,7 @@ monitoring.controller("controller", ['$scope', 'uiGmapGoogleMapApi', 'userServic
             visible: true,
             forbidden: "#ff0303"
         }]
-    }];
+    }*/];
 
     $scope.saveClient = function (client) {
         if (client.new) {
@@ -120,31 +182,173 @@ monitoring.controller("controller", ['$scope', 'uiGmapGoogleMapApi', 'userServic
         }
     };
 
+    $scope.deleteClient = function (client) {
+        positionService.deleteDevice($scope.login.username, $scope.login.password, client)
+            .success(
+            function (data) {
+                for(var i = 0; i < $scope.clients.length; i++) {
+                    if ($scope.clients[i].mac == client.mac) {
+                        if ($scope.clients.length == 1) {
+                            $scope.clients = [];
+                        } else {
+                            $scope.clients.splice(i, 1);
+                        }
+                    }
+                }
+            }
+        )
+            .error(
+            function (data) {
+                alert('Das Löschen, des Clients ist fehlgeschlagen, bitte versuchen Sie es später erneut!')
+            }
+        );
+    };
+
     $scope.path = {
         visible: false,
         path: [
-        {latitude: 51.1489846, longitude: 14.9977131},
-        {latitude: 51.2, longitude: 14.9977131},
-        {latitude: 51.1489846, longitude: 15.0}
+
     ]};
 
     $scope.addButton = function() {
-        var mac = prompt("Geben Sie bitte die MAC-Adresse des neuen Geräts ein!", "00:00:00:00:00");
+        var mac = prompt("Geben Sie bitte die MAC-Adresse des neuen Geräts ein!", "00:00:00:00:00:00");
         if (mac != null) {
             $scope.addClient(mac);
         }
     };
 
+    $scope.editZoneClick = function (zone) {
+        zone.editable = true;
+        zone.oldName = zone.name;
+    };
+
     $scope.addZone = function(id) {
         for (var i = 0; i < $scope.clients.length; i++) {
             if ($scope.clients[i].id == id) {
-                $scope.clients[i].zones.push({'name': "Neue Zone", 'center': $scope.clients[i].coords, 'radius': 100, 'editable': true, 'visible': true, 'forbidden': "#ff0303"})
+                $scope.clients[i].zones.push({'name': "Neue Zone", 'center': $scope.map.center, 'radius': 100, 'editable': true, 'visible': true, 'forbidden': "#ff0303", 'oldName': "Neue Zone"});
+                zonenService.createZone($scope.login.username, $scope.login.password, $scope.clients[i].mac, "Neue Zone", $scope.clients[i].coords.longitude, $scope.clients[i].coords.latitude, true, 100)
+                    .success(function (data) {
+                    })
+                    .error(function (data) {
+                        alert("Anlegen der Zone fehlgeschlagen, bitte versuchen Sie es später erneut!");
+                    });
             }
         }
     };
 
+    $scope.saveZone = function (zone, client) {
+        zonenService.updateZone($scope.login.username, $scope.login.password, client.mac, zone.name, zone.oldName, zone.center.longitude, zone.center.latitude, zone.forbidden == $scope.forbidden, zone.radius)
+            .success(
+            function (data) {
+                zone.editable = false;
+            }
+        )
+            .error(
+            function (data) {
+                alert('Das Editieren der Zone war nicht erfolgreich, bitte versuchen Sie es später erneut!');
+            }
+        );
+    };
+
+    $scope.showPath = function (client) {
+        positionService.readPath($scope.login.username, $scope.login.password, client)
+            .success(
+            function (data) {
+                var parser = new DOMParser();
+                var xmlDoc = parser.parseFromString(data,"text/xml");
+
+                for (var i = 0; i < xmlDoc.getElementsByTagName("entry").length; i++) {
+                    var entry = xmlDoc.getElementsByTagName("entry")[i];
+
+                    $scope.path.path.push({'latitude': parseFloat(entry.childNodes[5].childNodes[0].data), 'longitude': parseFloat(entry.childNodes[3].childNodes[0].data)});
+                }
+
+                $scope.path.visible = true;
+            }
+        )
+            .error();
+    };
+
+    $scope.hidePath = function () {
+        $scope.path.visible = false;
+        $scope.path.path = [];
+    };
+
+    $scope.logout = function () {
+        $scope.clients = [];
+        $scope.path = [];
+        $scope.login = {};
+        $scope.loggedIn = false;
+    };
+
+    var poll = function () {
+        positionService.readAllDevices($scope.login.username, $scope.login.password)
+            .success(
+            function (data) {
+                var parser = new DOMParser();
+                var xmlDoc = parser.parseFromString(data,"text/xml");
+
+                for (var i = 0; i < xmlDoc.getElementsByTagName("device").length; i++) {
+                    var device = xmlDoc.getElementsByTagName("device")[i];
+
+                    if (device.childNodes[9].hasChildNodes()) {
+
+                    } else {
+                        for (var z = 0; z < $scope.clients.length; z++) {
+                            if ($scope.clients[z].mac == device.childNodes.item(1).firstChild.data) {
+                                if ($scope.clients[z].coords.latitude != parseFloat(device.childNodes[9].childNodes[5].childNodes[0].data) || $scope.clients[z].coords.longitude != parseFloat(device.childNodes[9].childNodes[3].childNodes[0].data)) {
+                                    $scope.clients[z].coords.latitude = parseFloat(device.childNodes[9].childNodes[5].childNodes[0].data);
+                                    $scope.clients[z].coords.longitude = parseFloat(device.childNodes[9].childNodes[3].childNodes[0].data);
+                                }
+                            }
+                        }
+                    }
+                }
+                setTimeout(function () {poll();}, 1000)
+            }
+        )
+            .error(
+            function (data) {
+                alert('Die Verbindung zum Server ist Fehlgeschlagen, bitte versuchen Sie es später erneut!')
+            }
+        );
+    };
+
+    $scope.deleteZone = function (zone, client) {
+        zonenService.deleteZone($scope.login.username, $scope.login.password, client.mac, zone.name)
+            .success(
+            function (data) {
+                for(var i = 0; i < $scope.clients.length; i++) {
+                    if ($scope.clients[i].mac == client.mac) {
+                        for (var z = 0; z < client.zones.length; z++) {
+                            if (client.zones[z].name == zone.name) {
+                                if ($scope.clients[i].zones.length == 1) {
+                                    $scope.clients[i].zones = [];
+                                } else {
+                                    $scope.clients[i].zones.splice(z, 1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        )
+            .error(
+            function (data) {
+                alert("Das Löschen der Zone ist fehlgeschlagen, bitte versuchen Sie es später erneut!")
+            }
+        );
+    };
+
     $scope.addClient = function(mac) {
-        var newId = $scope.clients[$scope.clients.length - 1].id + 1;
+        var newId;
+
+        if ($scope.clients.length == 0) {
+            newId = 1;
+        } else {
+            newId = $scope.clients[$scope.clients.length - 1].id + 1;
+        }
+
         $scope.clients.push({'id': newId, 'name': "neuer Client", 'mac': mac, 'color': "ff0303", 'edit': true, 'visible': false, 'new': true, 'coords': {'latitude': 1.0, 'longitude': 1.0}, 'zones': [] });
     };
 
--- services.js
+++ services.js
@@ -38,6 +38,29 @@ xmlmonitServices.factory('userService', ['$http',
 xmlmonitServices.factory('positionService', ['$http',
     function ($http) {
         var positionService = {
+            readAllDevices: function (username, password) {
+                return $http({
+                    method: "POST",
+                    url: serverLocation + "/readDevice",
+                    data: "<data><user><username>" + username + "</username><userpassword>" + password + "</userpassword></user><device></device></data>",
+                    headers: {
+                        "Content-Type": "application/xml"
+                    }
+                });
+            },
+            readDevice: function (username, password, mac) {
+
+            },
+            readPath: function (username, password, client) {
+                return $http({
+                    method: "POST",
+                    url: serverLocation + "/readPath",
+                    data: "<data><user><username>" + username + "</username><userpassword>" + password + "</userpassword></user><device><clientMac>" + client.mac + "</clientMac></device></data>",
+                    headers: {
+                        "Content-Type": "application/xml"
+                    }
+                });
+            },
             createDevice : function(user, password, mac, devicename, color) {
                 return $http({
                     method: "POST",
@@ -57,9 +80,66 @@ xmlmonitServices.factory('positionService', ['$http',
                         "Content-Type" : "application/xml"
                     }
                 });
+            },
+            deleteDevice : function(user, password, client) {
+                return $http({
+                    method: "POST",
+                    url: serverLocation + "/deleteDevice",
+                    data: "<data><user><username>" + user + "</username><userpassword>" + password + "</userpassword></user><device><clientMac>" + client.mac + "</clientMac></device></data>",
+                    headers: {
+                        "Content-Type" : "application/xml"
+                    }
+                });
             }
         };
 
         return positionService;
+    }
+]);
+
+xmlmonitServices.factory('zonenService', ['$http',
+    function ($http) {
+        var zonenService = {
+            createZone : function(user, password, mac, name, longitude, latitude, restricted, radius) {
+                return $http({
+                    method: "POST",
+                    url: serverLocation + "/createZone",
+                    data: "<data><user><username>" + user + "</username><userpassword>" + password + "</userpassword></user><device><clientMac>" + mac + "</clientMac></device><zone><name>" + name + "</name><restricted>" + restricted + "</restricted><longitude>" + longitude +"</longitude><latitude>" + latitude +"</latitude><radius>" + radius + "</radius></zone></data>",
+                    headers: {
+                        "Content-Type" : "application/xml"
+                    }
+                });
+            },
+            updateZone : function(user, password, mac, name, oldname, longitude, latitude, restricted, radius) {
+                var data;
+
+                if (name == oldname) {
+                    data = "<data><user><username>" + user + "</username><userpassword>" + password + "</userpassword></user><device><clientMac>" + mac + "</clientMac></device><zone><name>" + oldname + "</name></zone><update><restricted>" + restricted + "</restricted><longitude>" + longitude +"</longitude><latitude>" + latitude +"</latitude><radius>" + radius + "</radius></update></data>";
+                } else {
+                    data = "<data><user><username>" + user + "</username><userpassword>" + password + "</userpassword></user><device><clientMac>" + mac + "</clientMac></device><zone><name>" + oldname + "</name></zone><update><name>" + name + "</name><restricted>" + restricted + "</restricted><longitude>" + longitude +"</longitude><latitude>" + latitude +"</latitude><radius>" + radius + "</radius></update></data>";
+                }
+
+                return $http({
+                    method: "POST",
+                    url: serverLocation + "/updateZone",
+                    data: data,
+                    headers: {
+                        "Content-Type" : "application/xml"
+                    }
+                });
+            },
+            deleteZone : function(user, password, mac, name) {
+            return $http({
+                method: "POST",
+                url: serverLocation + "/deleteZone",
+                data: "<data><user><username>" + user + "</username><userpassword>" + password + "</userpassword></user><device><clientMac>" + mac + "</clientMac></device><zone><name>" + name + "</name></zone></data>",
+                headers: {
+                    "Content-Type" : "application/xml"
+                }
+            });
+        }
+        };
+
+        return zonenService;
     }
 ]);
